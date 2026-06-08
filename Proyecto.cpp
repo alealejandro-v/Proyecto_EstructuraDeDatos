372a158dbb1dd4cdf0fb4e707e77b500550b0d26
alealejandro-v <edgaralejandrovazquezharo@gmail.com>
2026-06-08 17:56:01 -0600

    Update Proyecto.cpp

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -9,6 +9,10 @@
 #include <stdlib.h>
 
 void menu();
+void crear();
+void lista();
+void modificar();
+void aplicar();
 
 int main(){
 	
@@ -19,22 +23,29 @@ int main(){
 
 void menu(){
 	int opc;
+	do{
 	printf("\n----Menu----\n");
 	printf("Generar un examen_________1\n");
 	printf("Lista de examenes_________2\n");
 	printf("Modificar un examen_______3\n");
 	printf("Aplicar un examen_________4\n");
 	printf("Salir_____________________5\n");
-	printf("Seleccione una opcion: ");
 	
-	scanf("%d",&opc);
-	
-	do{
+		printf("Seleccione una opcion: ");
+		scanf("%d",&opc);
+		getchar();
+		
 		switch(opc){
-			case 1: break;
+			case 1: crear();
+					break;
+					
 			case 2: break;
 			case 3: break;
 			case 4: break;
 		}
 	} while(opc != 5);
+}
+
+void crear(){
+
 }
